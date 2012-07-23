ff512ae300157c827e965c1feb65217fb62088f7
James Timmins <jtimmins@engineyard.com>
2012-07-22 21:11:16 -0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
 
   #Returns the gravatar for the given user
-  def gravatar_for(user, options = { size: 40 })
+  def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name      "James Timmins"
-    email     "jtimmins@usc.edu"
-    password  "tacotaco"
-    password_confirmation "tacotaco"
+    sequence(:name)   { |n|  "Person #{n}" }
+    sequence(:email)  { |n|  "person_#{n}@example.com"}
+    password  "foobar"
+    password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
   end
 end
