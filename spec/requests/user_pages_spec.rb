0a9636af8e479ba97671024d9d4d5c1f818f973e
James Timmins <jtimmins@engineyard.com>
2012-07-27 16:35:21 -0700

    leaving ey

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -132,6 +132,7 @@ describe "User pages" do
           sign_in admin
           visit users_path
         end
+        
         it { should have_link('delete', href: user_path(User.first)) }
         it "should be able to delete another user" do
           expect { click_link('delete') }.to change(User, :count).by(-1)
